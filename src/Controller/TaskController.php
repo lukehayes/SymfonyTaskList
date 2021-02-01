56d4c5a1c73b00c1d04d38e031c1dbda1f493d61
lukehayes <lukepurplehayes@gmail.com>
2021-02-01 18:03:56 +0000

    Added Task::Create method

--- src/Controller/TaskController.php
+++ src/Controller/TaskController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\Task;
 
 class TaskController extends AbstractController
 {
@@ -16,5 +17,23 @@ class TaskController extends AbstractController
         return $this->render('task/index.html.twig', [
             'controller_name' => 'TaskController',
         ]);
+    }
+
+    /**
+     * @Route("/tasks/create", name="create-task")
+     */
+    public function create() : Response
+    {
+        $entityManager = $this->getDoctrine()->getManager();
+        
+        $task = new Task();
+        $task->setTask("Refactor code on project");
+        $task->setCompleted(false);
+        $task->setNote("This is important!");
+
+        $entityManager->persist($task);
+        $entityManager->flush();
+
+        return new Response("New Task Created!");
     }
 }
