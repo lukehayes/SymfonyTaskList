6add928c858da98532023b0f4d8e6061d0c72a70
lukehayes <lukepurplehayes@gmail.com>
2021-02-03 14:45:29 +0000

    Added a basic but working version of TaskController::update()

--- src/Controller/TaskController.php
+++ src/Controller/TaskController.php
@@ -25,6 +25,26 @@ class TaskController extends AbstractController
     }
 
     /**
+     * @Route("/tasks/update/{id}", name="update-task")
+     */
+    public function update(int $id) : Response
+    {
+        $entityManager = $this->getDoctrine()->getManager();
+
+        $task = $this->getDoctrine()
+                 ->getRepository(Task::class)
+                 ->find($id);
+
+        if(!$task)
+            throw $this->createNotFoundException("The task with ID {$id} could not be found.");
+
+        $task->setNotes("");
+        $entityManager->flush();
+
+        return $this->redirectToRoute('tasks');
+    }
+
+    /**
      * @Route("/tasks/remove/{id}", name="destroy-task")
      */
     public function destroy(int $id) : Response
