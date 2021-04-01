83f6752dd4477bef7c14dbeb4bb1f63298c5ceb1
lukehayes <lukepurplehayes@gmail.com>
2021-04-01 16:23:43 +0100

    Added quick and dirty fixture class simply to create and load some database data.

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -4,6 +4,8 @@ namespace App\DataFixtures;
 
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use App\Entity\Task;
+use App\Entity\Category;
 
 class AppFixtures extends Fixture
 {
@@ -11,6 +13,47 @@ class AppFixtures extends Fixture
     {
         // $product = new Product();
         // $manager->persist($product);
+
+        $this->loadCategories($manager);
+        $this->loadTasks($manager);
+
+    }
+
+    /**
+     * Quick and dirty way to load the database with categories.
+     */
+    private function loadCategories(ObjectManager $manager)
+    {
+        $categories = ['Work', 'App Development', 'Lifestyle', 'Learning', 'Other'];
+
+        foreach($categories as $cat)
+        {
+            $category = new Category();
+            $category->setName($cat);
+            $manager->persist($category);
+        }
+
+        $manager->flush();
+    }
+
+    private function loadTasks(ObjectManager $manager)
+    {
+        $categories = ['Work', 'App Development', 'Lifestyle', 'Learning', 'Other'];
+
+        for($i = 0; $i <= 15; $i++)
+        {
+            $task = new Task();
+            $task->setTask("A simple task.");
+            $task->setNotes("An example note can be here.");
+
+            $category = new Category();
+            $category->setName(array_rand($categories));
+
+            $task->setCategory($category);
+
+            $manager->persist($task);
+            $manager->persist($category);
+        }
 
         $manager->flush();
     }
